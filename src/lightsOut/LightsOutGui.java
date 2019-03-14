6d3e87ae7494dceddfa276abc30a89a541283e51
nolan <nolan@144.35.81.36>
2019-03-13 18:41:08 -0600

    LightsOut

---
+++ src/lightsOut/LightsOutGui.java
@@ -0,0 +1,132 @@
+package lightsOut;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import java.awt.GridLayout;
+import javax.swing.JLayeredPane;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+
+public class LightsOutGui extends JFrame {
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LightsOutGui frame = new LightsOutGui();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	public LightsOutGui() {
+		setTitle("Lights Out");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(new GridLayout(0, 5, 0, 0));
+		
+		JButton button = new JButton("0");
+		panel.add(button);
+		
+		JButton button_1 = new JButton("1");
+		panel.add(button_1);
+		
+		JButton button_2 = new JButton("1");
+		panel.add(button_2);
+		
+		JButton button_3 = new JButton("1");
+		panel.add(button_3);
+		
+		JButton button_4 = new JButton("1");
+		panel.add(button_4);
+		
+		JButton button_5 = new JButton("1");
+		panel.add(button_5);
+		
+		JButton button_6 = new JButton("1");
+		panel.add(button_6);
+		
+		JButton button_7 = new JButton("1");
+		panel.add(button_7);
+		
+		JButton button_8 = new JButton("1");
+		panel.add(button_8);
+		
+		JButton button_9 = new JButton("1");
+		panel.add(button_9);
+		
+		JButton button_10 = new JButton("0");
+		panel.add(button_10);
+		
+		JButton button_11 = new JButton("1");
+		panel.add(button_11);
+		
+		JButton button_12 = new JButton("1");
+		panel.add(button_12);
+		
+		JButton button_13 = new JButton("1");
+		panel.add(button_13);
+		
+		JButton button_14 = new JButton("1");
+		panel.add(button_14);
+		
+		JButton button_15 = new JButton("0");
+		panel.add(button_15);
+		
+		JButton button_16 = new JButton("1");
+		panel.add(button_16);
+		
+		JButton button_17 = new JButton("1");
+		panel.add(button_17);
+		
+		JButton button_18 = new JButton("1");
+		panel.add(button_18);
+		
+		JButton button_19 = new JButton("1");
+		panel.add(button_19);
+		
+		JButton button_20 = new JButton("0");
+		panel.add(button_20);
+		
+		JButton button_21 = new JButton("1");
+		panel.add(button_21);
+		
+		JButton button_22 = new JButton("1");
+		panel.add(button_22);
+		
+		JButton button_23 = new JButton("1");
+		panel.add(button_23);
+		
+		JButton button_24 = new JButton("1");
+		panel.add(button_24);
+		
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.NORTH);
+		
+		JButton btnNewButton = new JButton("New Game");
+		panel_1.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Reset Game");
+		panel_1.add(btnNewButton_1);
+	}
+}
